fc499d8f901f29122a79a0d938fb8c6e81813d33
JIWOO KWON <wldn000829@gmail.com>
2025-11-05 20:22:22 +0900

    [Bronze II] Title: 음계, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.py
@@ -0,0 +1,9 @@
+num = list(map(int,input().split()))
+asc = [1,2,3,4,5,6,7,8]
+des = [8,7,6,5,4,3,2,1]
+if num == asc:
+    print('ascending')
+elif num == des:
+    print('descending')
+else:
+    print('mixed')
