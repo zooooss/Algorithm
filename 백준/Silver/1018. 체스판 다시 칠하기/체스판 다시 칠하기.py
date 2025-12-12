23c2bd4f16c8ee42772c560d134b129edafa2dbd
JIWOO KWON <wldn000829@gmail.com>
2025-12-12 14:57:58 +0900

    [Silver III] Title: 체스판 다시 칠하기, Time: 72 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -1,23 +1,21 @@
-n,m = map(int, input().split())
-chess = [list(input().strip()) for _ in range(n)]
-
-answer=[]
-for i in range(n-7):
-    for j in range(m-7):
-        countW=0
-        countB=0
+m,n=map(int,input().split())
+chess=[list(input().strip())for _ in range(m)]
+res=float('inf')
+for i in range(m-7):
+    for j in range(n-7):
+        countW,countB=0,0
         for x in range(8):
             for y in range(8):
-                current=chess[i+x][j+y]
-                if(x+y)%2==0:
-                    if current!= 'W':
-                        countW += 1
-                    if current != 'B':
-                        countB += 1
+                first = chess[i+x][j+y]
+                if (x+y)%2==0:
+                    if first != 'W':
+                        countW+=1
+                    if first != 'B':
+                        countB+=1
                 else:
-                    if current != 'B':
-                        countW += 1
-                    if current != 'W':
-                        countB += 1
-        answer.append(min(countW,countB))
-print(min(answer))
+                    if first != 'W':
+                        countB+=1
+                    if first != 'B':
+                        countW+=1
+        res = min(res,countW,countB)
+print(res)
